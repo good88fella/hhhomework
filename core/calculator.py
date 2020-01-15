6812e30a33444d517923a22472293e13afc46302
Kindra Timika <ktimika@su-g4.21-school.ru>
2020-01-15 17:57:09 +0300

    g

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,36 @@
 from decorators import cache_decorator
 
+
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '/':
+        return a / b
+    elif operation == '*':
+        return a * b
+    elif operation == '**':
+        return a ** b
+    else:
+        raise ValueError
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    try:
+        a = int(input('Введите число: '))  # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+        b = int(input('Введите число: '))
+        operation = input('Введите операцию: ')
 
-    print('Результат: ', calculator(a, b, operation))
+        print('Результат: ', calculator(a, b, operation))
+
+    except ValueError:
+        print("Incorrect input.\nUsage:\n"
+              "Введите число: [number]\n"
+              "Введите число: [number]\n"
+              "Введите операцию: [+, -, /, *, **]")
+    except ZeroDivisionError:
+        print("Division by zero")
